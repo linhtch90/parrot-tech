2e37a55c05140b8906705f47cb07fa3b272faacb
Linh Truong Cong Hong <linhtch90@gmail.com>
2020-09-08 19:58:10 +0700

    Updates

---
+++ precache-manifest.7645a1848ae70489bc197636f86a1da1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "594dd58afd24c910226ba1b8aa0785fd",
+    "url": "/parrot-tech/index.html"
+  },
+  {
+    "revision": "c129a520b4f02c40c5cd",
+    "url": "/parrot-tech/static/css/main.a6bb4e78.chunk.css"
+  },
+  {
+    "revision": "dfec8cf24bad6680e0b5",
+    "url": "/parrot-tech/static/js/2.f3f02479.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/parrot-tech/static/js/2.f3f02479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c129a520b4f02c40c5cd",
+    "url": "/parrot-tech/static/js/main.27b2c50a.chunk.js"
+  },
+  {
+    "revision": "5b3797e58151c3a31d99",
+    "url": "/parrot-tech/static/js/runtime-main.aae1f8dc.js"
+  }
+]);
